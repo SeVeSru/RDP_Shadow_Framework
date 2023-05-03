d12eb18287f2f58c374ba6ce81fefc8b947e3070
SeVeSru <qwertysp96@gmail.com>
2023-05-03 12:59:48 +0700

    Добавьте файлы проекта.

---
+++ RDP_Shadow_Framework/Form1.Designer.cs
@@ -0,0 +1,186 @@
+﻿using System.Windows.Forms;
+
+namespace RDP_Shadow_Framework
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.sessionListView = new System.Windows.Forms.ListView();
+            this.Username = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.SessionID = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.refreshButton = new System.Windows.Forms.Button();
+            this.shadowButton = new System.Windows.Forms.Button();
+            this.AutoUpdate = new System.Windows.Forms.CheckBox();
+            this.checkControl = new System.Windows.Forms.CheckBox();
+            this.serverComboBox = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // sessionListView
+            // 
+            this.sessionListView.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.Username,
+            this.SessionID,
+            this.columnHeader1,
+            this.columnHeader2});
+            this.sessionListView.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.sessionListView.FullRowSelect = true;
+            this.sessionListView.HideSelection = false;
+            this.sessionListView.Location = new System.Drawing.Point(0, 0);
+            this.sessionListView.Margin = new System.Windows.Forms.Padding(0, 0, 0, 0);
+            this.sessionListView.Name = "sessionListView";
+            this.sessionListView.Size = new System.Drawing.Size(405, 703);
+            this.sessionListView.Sorting = System.Windows.Forms.SortOrder.Ascending;
+            this.sessionListView.TabIndex = 9;
+            this.sessionListView.UseCompatibleStateImageBehavior = false;
+            this.sessionListView.View = System.Windows.Forms.View.Details;
+            this.sessionListView.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.sessionListView_MouseDoubleClick);
+            // 
+            // Username
+            // 
+            this.Username.Text = "Имя пользователя";
+            this.Username.Width = 100;
+            // 
+            // SessionID
+            // 
+            this.SessionID.Text = "ID";
+            this.SessionID.Width = 0;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.DisplayIndex = 3;
+            this.columnHeader1.Text = "Активный";
+            this.columnHeader1.Width = 0;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.DisplayIndex = 2;
+            this.columnHeader2.Text = "ФИО";
+            this.columnHeader2.Width = 300;
+            // 
+            // refreshButton
+            // 
+            this.refreshButton.AutoSize = true;
+            this.refreshButton.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.refreshButton.Location = new System.Drawing.Point(0, 672);
+            this.refreshButton.Margin = new System.Windows.Forms.Padding(4);
+            this.refreshButton.Name = "refreshButton";
+            this.refreshButton.Size = new System.Drawing.Size(405, 31);
+            this.refreshButton.TabIndex = 10;
+            this.refreshButton.Text = "Обновить (F5)";
+            this.refreshButton.UseVisualStyleBackColor = true;
+            this.refreshButton.Click += new System.EventHandler(this.refreshButton_Click_1);
+            // 
+            // shadowButton
+            // 
+            this.shadowButton.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.shadowButton.Location = new System.Drawing.Point(0, 642);
+            this.shadowButton.Margin = new System.Windows.Forms.Padding(4);
+            this.shadowButton.Name = "shadowButton";
+            this.shadowButton.Size = new System.Drawing.Size(405, 30);
+            this.shadowButton.TabIndex = 11;
+            this.shadowButton.Text = "Подключится";
+            this.shadowButton.UseVisualStyleBackColor = true;
+            this.shadowButton.Click += new System.EventHandler(this.shadowButton_Click_1);
+            // 
+            // AutoUpdate
+            // 
+            this.AutoUpdate.AutoSize = true;
+            this.AutoUpdate.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.AutoUpdate.Location = new System.Drawing.Point(0, 604);
+            this.AutoUpdate.Margin = new System.Windows.Forms.Padding(2);
+            this.AutoUpdate.Name = "AutoUpdate";
+            this.AutoUpdate.Size = new System.Drawing.Size(405, 19);
+            this.AutoUpdate.TabIndex = 16;
+            this.AutoUpdate.Text = "Автообновление списка каждые 10 сек";
+            this.AutoUpdate.UseVisualStyleBackColor = true;
+            this.AutoUpdate.CheckedChanged += new System.EventHandler(this.AutoUpdate_CheckedChanged);
+            // 
+            // checkControl
+            // 
+            this.checkControl.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.checkControl.Location = new System.Drawing.Point(0, 623);
+            this.checkControl.Name = "checkControl";
+            this.checkControl.Size = new System.Drawing.Size(405, 19);
+            this.checkControl.TabIndex = 17;
+            this.checkControl.Text = "Управление";
+            this.checkControl.UseVisualStyleBackColor = true;
+            // 
+            // serverComboBox
+            // 
+            this.serverComboBox.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.serverComboBox.FormattingEnabled = true;
+            this.serverComboBox.Location = new System.Drawing.Point(0, 581);
+            this.serverComboBox.Name = "serverComboBox";
+            this.serverComboBox.Size = new System.Drawing.Size(405, 23);
+            this.serverComboBox.TabIndex = 19;
+            this.serverComboBox.Text = "Ваш сервер";
+            this.serverComboBox.SelectedIndexChanged += new System.EventHandler(this.refreshButton_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(405, 703);
+            this.Controls.Add(this.serverComboBox);
+            this.Controls.Add(this.AutoUpdate);
+            this.Controls.Add(this.checkControl);
+            this.Controls.Add(this.shadowButton);
+            this.Controls.Add(this.refreshButton);
+            this.Controls.Add(this.sessionListView);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.KeyPreview = true;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.MaximizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(421, 742);
+            this.Name = "Form1";
+            this.Text = "Запаска RDP";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private ListView sessionListView;
+        private Button refreshButton;
+        private Button shadowButton;
+        private ColumnHeader SessionID;
+        private ColumnHeader Username;
+        private CheckBox AutoUpdate;
+        private ColumnHeader columnHeader1;
+        private ColumnHeader columnHeader2;
+        private CheckBox checkControl;
+        private ComboBox serverComboBox;
+    }
+}
+
---
+++ RDP_Shadow_Framework/Form1.cs
@@ -0,0 +1,206 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.DirectoryServices;
+using System.IO;
+using System.Windows.Forms;
+
+namespace RDP_Shadow_Framework
+{
+    public partial class Form1 : Form
+    {
+        private static readonly Timer timer = new Timer();
+        private readonly int  timerInterval = 10000; // интервер в мс обновления списка
+        private readonly string stDomen = Environment.UserDomainName; // Домен
+        //private readonly string  stServer = "192.168.11.225"; // Сервер
+
+        public Form1()
+        {
+            InitializeComponent();
+            
+            LDAP_server();
+
+            //RefreshSessionList();
+
+            // Устанавливаем интервал таймера в милисекундах
+            timer.Interval = timerInterval;
+
+            // Добавляем обработчик события Tick
+            timer.Tick += refreshButton_Click_1;
+        }
+
+        private void LDAP_server()
+        {
+            DirectoryEntry entry = new DirectoryEntry("LDAP://DC=" + stDomen + ",DC=loc"); // replace with your AD server address
+            DirectorySearcher searcher = new DirectorySearcher(entry);
+            searcher.Filter = "(&(objectClass=computer)(operatingSystem=*Server*))"; // filter for server computers
+
+            SearchResultCollection results = searcher.FindAll(); // execute search and get results
+            List<string> serverNames = new List<string>();
+
+            foreach (SearchResult result in results)
+            {
+                string serverName = result.Properties["Name"][0].ToString(); // get server name
+                serverNames.Add(serverName); // add server name to List<string>
+            }
+
+            serverNames.Sort(); // sort alphabetically
+
+            foreach (string serverName in serverNames)
+            {
+                serverComboBox.Items.Add(serverName); // add server name to ComboBox
+            }
+        }
+
+        private string GetFullNameByLogin(string login)
+        {
+            using (var entry = new DirectoryEntry($"LDAP://{stDomen}"))
+            {
+                using (var search = new DirectorySearcher(entry))
+                {
+                    search.Filter = $"(&(objectClass=user)(sAMAccountName={login}))";
+                    search.PropertiesToLoad.Add("displayName");
+                    var result = search.FindOne();
+                    if (result != null)
+                    {
+                        return result.Properties["displayName"][0].ToString();
+                    }
+                    else
+                    {
+                        return "";
+                    }
+                }
+            }
+        }
+        public void RefreshSessionList()
+        {
+            // Создаем словарь, где ключ - идентификатор сессии, а значение - элемент списка
+            var sessionDict = new Dictionary<string, ListViewItem>();
+            foreach (ListViewItem item in sessionListView.Items)
+            {
+                if (item.SubItems.Count >= 2)
+                {
+                    sessionDict[item.SubItems[1].Text] = item;
+                }
+            }
+            string queryPath = string.Empty;
+            if (Environment.Is64BitOperatingSystem && !Environment.Is64BitProcess)
+                queryPath = Path.Combine(Environment.GetEnvironmentVariable("windir"), "Sysnative", "query.exe");
+            else
+                queryPath = Path.Combine(Environment.GetEnvironmentVariable("windir"), "System32", "query.exe");
+            string stServer = serverComboBox.Text;
+            Process process = new Process();
+            process.StartInfo.FileName = queryPath;
+            process.StartInfo.Arguments = $"session /server:{stServer}";
+            process.StartInfo.RedirectStandardOutput = true;
+            process.StartInfo.UseShellExecute = false;
+            process.StartInfo.Domain = Environment.UserDomainName;
+            process.StartInfo.CreateNoWindow = true;
+            string output = "";
+            process.Start();
+            bool processExited = process.WaitForExit(5000); // ожидание 5 секунд
+            if (processExited)
+            {
+                output = process.StandardOutput.ReadToEnd();
+            }
+            else
+            {
+                process.Kill(); // Убивает процесс
+                MessageBox.Show("Сервер не отвечает", "Сервер не отвечает", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+            // Обновляем или добавляем элементы списка
+            string[] lines = output.Split(new[] { '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries);
+            for (int i = 2; i < lines.Length - 1; i++)
+            {
+                string[] fields = lines[i].Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+                if (fields.Length >= 3 && fields[0].Contains("rdp-tcp#"))
+                {
+                    string sessionId = fields[2];
+                    ListViewItem item;
+                    if (sessionDict.ContainsKey(sessionId))
+                    {
+                        // Обновляем значения элемента списка
+                        item = sessionDict[sessionId];
+                        if (item.SubItems.Count >= 3) // Проверяем, что элемент существует
+                        {
+                            item.SubItems[0].Text = fields[1];
+                            item.SubItems[2].Text = fields[3];
+                        }
+                    }
+                    else
+                    {
+                        // Добавляем новый элемент в список
+                        item = new ListViewItem(fields[1]);
+                        item.SubItems.Add(sessionId);
+                        item.SubItems.Add(fields.Length >= 4 ? fields[3] : ""); // Проверяем, что элемент существует
+
+                        // Получаем ФИО пользователя и добавляем его в колонку списка
+                        string login = fields[1].Substring(fields[1].IndexOf("\\") + 1);
+                        string fullName = GetFullNameByLogin(login);
+                        item.SubItems.Add(fullName);
+
+                        sessionListView.Items.Add(item);
+                    }
+                    // Удаляем сессию из словаря, чтобы в конце остались только удаленные сессии
+                    sessionDict.Remove(sessionId);
+                }
+            }
+
+            // Удаляем элементы списка, соответствующие удаленным сессиям
+            foreach (var pair in sessionDict)
+            {
+                sessionListView.Items.Remove(pair.Value);
+            }
+        }
+
+        private void refreshButton_Click_1(object sender, EventArgs e)
+        {
+            RefreshSessionList();
+        }
+
+        private void shadowButton_Click_1(object sender, EventArgs e)
+        {
+            if (sessionListView.SelectedItems.Count > 0 && sessionListView.SelectedItems[0].SubItems.Count > 1)
+            {
+                int sessionId = int.Parse(sessionListView.SelectedItems[0].SubItems[1].Text);
+                Process process = new Process();
+                process.StartInfo.FileName = "mstsc.exe";
+                string Options = "/noConsentPrompt";
+                if (checkControl.Checked)
+                {
+                    Options = Options + " /control";
+                }
+                process.StartInfo.Arguments = $"/shadow:{sessionId} /v:RDP1 {Options}";
+
+                process.Start();
+            }
+        }
+
+        private void sessionListView_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            shadowButton_Click_1(sender, e);
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.F5)
+            {
+                RefreshSessionList();
+                e.Handled = true; // Это предотвращает дальнейшую обработку нажатия клавиши F5 формой
+            }
+        }
+
+        private void AutoUpdate_CheckedChanged(object sender, EventArgs e)
+        {
+            if (AutoUpdate.Checked)
+            {
+                timer.Start();
+            }
+            else
+            {
+                timer.Stop();
+            }
+        }
+    }
+}
